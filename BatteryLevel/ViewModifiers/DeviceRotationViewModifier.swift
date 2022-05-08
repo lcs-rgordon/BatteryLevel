bdd8c788adf16c8302a3992c911516bae08e1835
Russell Gordon <github@russellgordon.ca>
2022-05-08 08:09:18 -0400

    Added code to receive notification from UIDevice API for when device rotation changes.

---
+++ BatteryLevel/ViewModifiers/DeviceRotationViewModifier.swift
@@ -0,0 +1,32 @@
+//
+//  DeviceRotationViewModifier.swift
+//  BatteryLevel
+//
+//  Created by Russell Gordon on 2022-05-08.
+//
+
+import Foundation
+import SwiftUI
+
+// SOURCE:
+// https://www.hackingwithswift.com/quick-start/swiftui/how-to-detect-device-rotation
+// Our custom view modifier to track rotation and
+// call our action
+struct DeviceRotationViewModifier: ViewModifier {
+    let action: (UIDeviceOrientation) -> Void
+
+    func body(content: Content) -> some View {
+        content
+            .onAppear()
+            .onReceive(NotificationCenter.default.publisher(for: UIDevice.orientationDidChangeNotification)) { _ in
+                action(UIDevice.current.orientation)
+            }
+    }
+}
+
+// A View wrapper to make the modifier easier to use
+extension View {
+    func onRotate(perform action: @escaping (UIDeviceOrientation) -> Void) -> some View {
+        self.modifier(DeviceRotationViewModifier(action: action))
+    }
+}
--- BatteryLevel/Views/ContentView.swift
+++ BatteryLevel/Views/ContentView.swift
@@ -12,9 +12,28 @@ struct ContentView: View {
     // MARK: Stored properties
     @State var currentBatteryLevel: Float = 0.0
     
+    // Will be populated with the current device orientation
+    @State private var orientation = UIDeviceOrientation.unknown
+    
     var body: some View {
         VStack {
             Text("Current battery level is: \(currentBatteryLevel)")
+            
+            Group {
+                if orientation.isPortrait {
+                    Text("Portrait")
+                } else if orientation.isLandscape {
+                    Text("Landscape")
+                } else if orientation.isFlat {
+                    Text("Flat")
+                } else {
+                    Text("Unknown")
+                }
+            }
+            // Uses the custom view modifier to receive the notifications for when the device orientation has changed
+            .onRotate { newOrientation in
+                orientation = newOrientation
+            }
         }
         .task {
             
