467e8a5e6ccda4e1492b2186227a40947d101f18
Russell Gordon <github@russellgordon.ca>
2022-05-08 07:42:42 -0400

    Initial Commit

---
+++ BatteryLevel/BatteryLevelApp.swift
@@ -0,0 +1,17 @@
+//
+//  BatteryLevelApp.swift
+//  BatteryLevel
+//
+//  Created by Russell Gordon on 2022-05-08.
+//
+
+import SwiftUI
+
+@main
+struct BatteryLevelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
